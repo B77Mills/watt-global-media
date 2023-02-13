768bd2b634ec1b61a1fca695fbb8a85d9d99c520
Brandon Krigbaum <brandonbk@gmail.com>
2023-02-13 09:45:30 -0600

    Fix corporate logo path

--- packages/global/config/corporate.js
+++ packages/global/config/corporate.js
@@ -3,9 +3,9 @@ module.exports = {
   logo: {
     alt: 'WATT Global Media Logo',
     href: 'https://www.wattglobalmedia.com/',
-    src: 'https://p1-cms-assets.imgix.net/files/base/p1/sandbox/image/static/WATTglobal_4c_k_R_REV.png?w=200&auto=format,compress',
+    src: 'https://p1-cms-assets.imgix.net/files/base/wattglobalmedia/all/image/static/WATTglobal_4c_k_R_REV.png?w=200&auto=format,compress',
     srcset: [
-      'https://p1-cms-assets.imgix.net/files/base/p1/sandbox/image/static/WATTglobal_4c_k_R_REV.png?w=200&auto=format,compress&dpr=2 2x',
+      'https://p1-cms-assets.imgix.net/files/base/wattglobalmedia/all/image/static/WATTglobal_4c_k_R_REV.png?w=200&auto=format,compress&dpr=2 2x',
     ],
   },
 };
