cd6a593facdd760093cdf115fdea3fff4a8e1211
Brandon Krigbaum <brandonbk@gmail.com>
2023-02-12 18:23:43 -0600

    Add corporate config

---
+++ packages/global/config/corporate.js
@@ -0,0 +1,11 @@
+module.exports = {
+  company: 'WATT Global Media',
+  logo: {
+    alt: 'WATT Global Media Logo',
+    href: 'https://www.wattglobalmedia.com/',
+    src: 'https://p1-cms-assets.imgix.net/files/base/p1/sandbox/image/static/WATTglobal_4c_k_R_REV.png?w=200&auto=format,compress',
+    srcset: [
+      'https://p1-cms-assets.imgix.net/files/base/p1/sandbox/image/static/WATTglobal_4c_k_R_REV.png?w=200&auto=format,compress&dpr=2 2x',
+    ],
+  },
+};
