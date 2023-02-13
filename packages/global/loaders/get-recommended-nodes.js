a9a1726261d73f8de92225ed7b48e8627c4d094f
Brandon Krigbaum <brandonbk@gmail.com>
2023-02-13 09:45:15 -0600

    Add loaders

---
+++ packages/global/loaders/get-recommended-nodes.js
@@ -0,0 +1,16 @@
+const { websiteScheduledContent: query } = require('@parameter1/base-cms-web-common/block-loaders');
+const defaultFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/content-list');
+
+module.exports = async (apolloClient, paramsSets = []) => {
+  if (!paramsSets || !paramsSets.length) return { nodes: [] };
+  const results = await Promise.all(paramsSets.map(params => query(apolloClient, {
+    queryFragment: defaultFragment,
+    ...params,
+  })));
+
+  const nodes = results.reduce((arr, result) => {
+    arr.push(...result.nodes);
+    return arr;
+  }, []);
+  return { nodes };
+};
