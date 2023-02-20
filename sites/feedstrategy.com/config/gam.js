ae287ad0d0d7ee1d8fad74ce85bd9577695d32c7
Brandon Krigbaum <brandonbk@gmail.com>
2023-02-20 09:14:04 -0600

    Move lazyload config to global

--- sites/feedstrategy.com/config/gam.js
+++ sites/feedstrategy.com/config/gam.js
@@ -2,13 +2,6 @@ const configureGAM = require('@watt-global-media/package-global/config/gam');
 
 const config = configureGAM({ basePath: 'p1' });
 
-config.lazyLoad = {
-  enabled: true, // set to true to enable lazy loading
-  fetchMarginPercent: 100, // fetch ad when one viewport away
-  renderMarginPercent: 50, // render ad when half viewport away
-  mobileScaling: 2, // double these on mobile
-};
-
 config.setAliasAdUnits('default', [
   { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'leaderboard' },
   { name: 'rotation', templateName: 'ROTATION', path: 'rotation' },
